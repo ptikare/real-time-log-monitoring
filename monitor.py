daa0f1ba5002948d33df0b1549c93fe3ef4d4b72
Praneeta Tikare <pranitatikare9@gmail.com>
2026-04-13 19:57:30 +0530

    Added log parsing with timestamp and log level extraction

--- monitor.py
+++ monitor.py
@@ -1,9 +1,24 @@
+import re
+from datetime import datetime
+
+def parse_log(line):
+  pattern = r"^(.*?)\s+(INFO|ERROR|WARN)"
+  match = re.match(pattern, line)
+
+  if match:
+    timestamp_str, level = match.groups()
+    timestamp = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S")
+    return timestamp, level
+return None, None
+
+
 def analyze_logs(file_path):
   error_count = 0
 
   with open(file_path, "r") as file:
     for line in file:
-      if "ERROR" in line:
+      _, level = parse_log(line)
+      if level == "ERROR":
         error_count += 1
 
   print(f"Total ERROR logs: {error_count}")
@@ -11,4 +26,3 @@ def analyze_logs(file_path):
 
 if __name__ == "__main__":
   analyze_logs("app.log")
-  
