02530abc617398f7c6fe1f837611fe9b90644c5d
Praneeta Tikare <pranitatikare9@gmail.com>
2026-05-03 20:07:36 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,24 +1,21 @@
-import time
 from prometheus_client import start_http_server, Counter, Gauge
+import time
+import os
 
-ERROR_THRESHOLD = 5
-alert_triggered = False
-error_count = 0
-total_count_local = 0
-start_time = time.time()
-
-# Prometheus metrics
 error_counter = Counter('error_count', 'Total number of error logs')
 log_lines_total = Counter('log_lines_total', 'Total log lines processed')
+log_levels = Counter('log_levels_total', 'Log levels count', ['level'])
 error_rate = Gauge('error_rate', 'Error rate (errors per second)')
 
-def monitor(file_path):
-    global error_count, alert_triggered
-    
-    print("Monitoring with alert thresholds...")
 
-    # Start Prometheus metrics server
-    start_http_server(8000, addr="0.0.0.0")
+def monitor(file_path):
+    while not os.path.exists(file_path):
+        print("Waiting for log file...")
+        time.sleep(1)
+
+    error_count_local = 0
+    total_count_local = 0
+    start_time = time.time()
 
     with open(file_path, "r") as file:
         file.seek(0, 2)
@@ -35,15 +32,20 @@ def monitor(file_path):
 
             if "ERROR" in line:
                 error_counter.inc()
+                log_levels.labels(level="error").inc()
                 error_count_local += 1
 
-            if error_count >= ERROR_THRESHOLD and not alert_triggered:
-                print(f"🚨 ALERT: High error count ({error_count})")
-                alert_triggered = True
+            elif "INFO" in line:
+                log_levels.labels(level="info").inc()
 
-            if error_count < ERROR_THRESHOLD:
-                alert_triggered = False
+            elif "WARNING" in line:
+                log_levels.labels(level="warning").inc()
+
+            elapsed_time = time.time() - start_time
+            if elapsed_time > 0:
+                error_rate.set(error_count_local / elapsed_time)
 
 
 if __name__ == "__main__":
+    start_http_server(8000, addr="0.0.0.0")
     monitor("app.log")
